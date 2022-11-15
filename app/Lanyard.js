db71e1735c2f59740ef81bc4915b16290f6482ab
mertushka <mertushkaonline@gmail.com>
2022-11-15 15:01:17 +0300

    fix: seo & speed

--- app/Lanyard.js
+++ app/Lanyard.js
@@ -21,50 +21,53 @@ function App() {
     <>
       {!loading && (
         <>
-          <div class="intro">
-            <div id="profile">
-              <Image
-                id="avatar"
-                src={
-                  "https://cdn.discordapp.com/avatars/" +
-                  status.discord_user.id +
-                  "/" +
-                  status.discord_user.avatar
-                }
-                width={128}
-                height={128}
-                alt="mertushka"
-                priority={true}
-                className="shadow-dark"
-              />
-              <h1 id="username" style={rubik.style}>
-                {status.discord_user.username +
-                  "#" +
-                  status.discord_user.discriminator}
-              </h1>
-              <p>Backend Developer</p>
-              <div className="social-links">
-                <a href="https://github.com/mertushka" target="_blank">
-                  <svg className="icon icon-github">
-                    <use xlinkHref="#icon-github"></use>
-                  </svg>
-                </a>
-              </div>
-              {status.activities?.find((a) => a.type === 0) == undefined || (
-                <iframe
-                  id="discord-presence"
-                  style={{
-                    border: "none",
-                    overflow: "hidden",
-                    borderRadius: "10px",
-                    margin: "10px",
-                  }}
-                  src="https://lanyard-visualizer.netlify.app/user/940131816692674591?background=false&title=false&mode=iframe"
-                  width="50%"
-                  height="144px"
-                ></iframe>
-              )}
+          <div id="profile">
+            <Image
+              id="avatar"
+              src={
+                "https://cdn.discordapp.com/avatars/" +
+                status.discord_user.id +
+                "/" +
+                status.discord_user.avatar
+              }
+              width={128}
+              height={128}
+              alt="mertushka"
+              priority={true}
+              loading="eager"
+              className="shadow-dark"
+            />
+            <h1 id="username" style={rubik.style}>
+              {status.discord_user.username +
+                "#" +
+                status.discord_user.discriminator}
+            </h1>
+            <p>Backend Developer</p>
+            <div className="social-links">
+              <a
+                href="https://github.com/mertushka"
+                aria-label="Github Profile"
+                target="_blank"
+              >
+                <svg className="icon icon-github">
+                  <use xlinkHref="#icon-github"></use>
+                </svg>
+              </a>
             </div>
+            {status.activities?.find((a) => a.type === 0) == undefined || (
+              <iframe
+                id="discord-presence"
+                style={{
+                  border: "none",
+                  overflow: "hidden",
+                  borderRadius: "10px",
+                  margin: "10px",
+                }}
+                src="https://lanyard-visualizer.netlify.app/user/940131816692674591?background=false&title=false&mode=iframe"
+                width="50%"
+                height="144px"
+              ></iframe>
+            )}
           </div>
         </>
       )}
